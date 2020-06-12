de2fef3ddd2403940d9063b2e73ac777b84289fb
FritzJo <fritzjo-git@mailbox.org>
2020-06-12 12:58:29 +0200

    added module for progressbar

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ app = Flask(__name__)
 def test(image_name):
     layout_name = request.args.get('layout')
     if layout_name is None or not os.path.isfile('static/layouts/' + layout_name + '.json'):
-        return "Please specify a valid layout!"
+        if os.path.isfile('static/layouts/' + image_name + '.json'):
+            layout_name = image_name
+        else:
+            return "Please specify a valid layout!"
     config = load_config(layout_name)
 
     if not os.path.isfile('static/images/' + image_name + '.png'):
@@ -30,11 +33,42 @@ def test(image_name):
 
     output = io.BytesIO()
 
-    for overlays in config['extras']:
-        offset = tuple(overlays['offset'])
-        image_file = overlays['filename']
-        bar = Image.open('static/images/' + image_file)
-        img.paste(bar, offset)
+    for extra in config['extras']:
+        if extra['type'] == "image":
+            offset = tuple(extra['offset'])
+            image_file = extra['filename']
+            bar = Image.open('static/images/' + image_file)
+            img.paste(bar, offset)
+        if extra['type'] == "progressbar":
+            # Add progressbar background
+            offset = tuple(extra['position_bar'])
+            image_file = extra['filename_bar']
+            bar = Image.open('static/images/' + image_file)
+            img.paste(bar, offset)
+
+            position_bar = tuple(extra['position_bar'])
+            filename_bar = extra['filename_bar']
+            bar = Image.open('static/images/' + filename_bar)
+            width, height = bar.size
+            width_pointer = extra['width_pointer']
+            pointer_color = tuple(extra['color_pointer'])
+
+            # x0, y0, x1, y1
+            x_offset_pointer = position_bar[0]
+            y_offset_pointer = position_bar[1]
+
+            shape = [0 + x_offset_pointer,
+                     height + y_offset_pointer,
+                     width_pointer + x_offset_pointer,
+                     0 + y_offset_pointer]
+            # Calculate value of progressbar
+            ratio = width / extra['max']
+            progress_value = float(request.args.get(extra['value_parameter_name'])) * ratio
+
+            # Set value
+            shape[0] = shape[0] + progress_value
+            shape[2] = shape[2] + progress_value
+            draw.rectangle(shape, fill=pointer_color)
 
     img.convert('RGBA').save(output, format='PNG')
     output.seek(0, 0)
