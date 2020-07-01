cac57e2f674d5081d4bb1b47308962317c159511
FritzJo <fritzjo-git@mailbox.org>
2020-07-01 14:04:34 +0200

    removed unused imports

--- image.py
+++ image.py
@@ -1,7 +1,5 @@
 from PIL import Image
 
-from storage import get_image
-
 # https://stackoverflow.com/questions/273946/how-do-i-resize-an-image-using-pil-and-maintain-its-aspect-ratio
 def scale_image(img, target_height):
     hpercent = (target_height / float(img.size[1]))
