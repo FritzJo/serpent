1eb2610e49e33865d7593a69cc9d4061496bcb5b
FritzJo <fritzjo-git@mailbox.org>
2020-09-08 21:24:50 +0200

    fixed syntax

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def process_image(image_name):
                 orientation = "horizontal"
 
             bar = get_image(filename_bar)
-            progress_parameter_value = request.args.get(extra['value_parameter_name']
+            progress_parameter_value = request.args.get(extra['value_parameter_name'])
             if orientation == "horizontal":
                 # Scale pointer to fit varimage box (width + height) while keeping the aspect ratio
                 bar = scale_image(bar, height)
@@ -68,7 +68,7 @@ def process_image(image_name):
                 # Move position of pointer
                 offset = list(offset)
                 ratio = width / max_v
-                value = min(progress_parameter_value), max_v)
+                value = min(progress_parameter_value, max_v)
                 progress_value = float(value) * ratio
                 offset[0] += int(progress_value)
                 offset = tuple(offset)
@@ -78,7 +78,7 @@ def process_image(image_name):
                 # Move position of pointer
                 offset = list(offset)
                 ratio = height / max_v
-                value = min(progress_parameter_value), max_v)
+                value = min(progress_parameter_value, max_v)
                 progress_value = extra['max'] - float(value) * ratio
                 offset[1] += int(progress_value)
                 offset = tuple(offset)
