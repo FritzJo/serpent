a41d6105e0112f505bb0e776d7a4c5e34f334ce3
FritzJo <fritzjo-git@mailbox.org>
2020-09-15 21:27:02 +0200

    added file changes to test cases

--- test/test_image.py
+++ test/test_image.py
@@ -2,34 +2,34 @@ import unittest
 
 from PIL import Image
 
-from image import scale_image
+from src.image import scale_image
 
 
 class TestScaling(unittest.TestCase):
     def test_no_scaling(self):
-        img = Image.open('../static/images/example.png')
+        img = Image.open('../src/static/images/example.png')
         current_width, current_height = img.size
         img_scaled = scale_image(img, current_height)
         self.assertEqual(img.size, img_scaled.size)
 
     def test_50_scaling(self):
-        img = Image.open('../static/images/example.png')
+        img = Image.open('../src/static/images/example.png')
         current_width, current_height = img.size
         img_scaled = scale_image(img, current_height / 2)
         self.assertEqual(img.size[0] / 2, img_scaled.size[0])
         self.assertEqual(img.size[1] / 2, img_scaled.size[1])
 
     def test_150_scaling(self):
-        img = Image.open('../static/images/example.png')
+        img = Image.open('../src/static/images/example.png')
         current_width, current_height = img.size
         img_scaled = scale_image(img, current_height * 2)
         self.assertEqual(img.size[0] * 2, img_scaled.size[0])
         self.assertEqual(img.size[1] * 2, img_scaled.size[1])
 
     def test_0_scaling(self):
-        img = Image.open('../static/images/example.png')
+        img = Image.open('../src/static/images/example.png')
         self.assertRaises(ValueError, scale_image, img, 0)
 
     def test_negative_scaling(self):
-        img = Image.open('../static/images/example.png')
+        img = Image.open('../src/static/images/example.png')
         self.assertRaises(ValueError, scale_image, img, -100)
